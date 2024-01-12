a4215ef0f02bc65e7d7803383de531626a0b2464
leogmc <ifbaianoleo@gmail.com>
2024-01-12 00:46:23 -0300

    Aprimorando o exercício DadosEstatisticos

--- 08a12deJaneiro/Exercises/DadosEstatisticos/src/dadosestatisticos/App.java
+++ 08a12deJaneiro/Exercises/DadosEstatisticos/src/dadosestatisticos/App.java
@@ -1,22 +1,39 @@
 package dadosestatisticos;
-
-import java.util.ArrayList;
 import java.util.Date;
 
 public class App {
 
 	public static void main(String[] args) {
+			
+		// Objeto Data
+		DatasDeNascimento datas = new DatasDeNascimento();		
+		
+		//OBS: O formato Date conta os meses de 0 até 11, sendo 0 Janeiro e 11 Dezembro (maluquice)
+		Date data1 = new Date(2002,0,15);
+		Date data2 = new Date(2003,11,06);
+		Date data3 = new Date(2023,05,20);
+		
+		datas.listaDatas.add(data3);
+		datas.listaDatas.add(data1);
+		datas.listaDatas.add(data2);
+			
+		System.out.println("Maior data: ");
+		System.out.println(datas.max());
+		System.out.println(" ");
+		
+		System.out.println("Menor data: ");	
+		System.out.println(datas.min());
+		System.out.println(" ");
+		
+		datas.ordernar();
+		
+		datas.buscar(data3);
 		
 		
 		
-		// Criando objetos das classes
-		DatasDeNascimento datas = new DatasDeNascimento();
-		
-		ArrayList<Date> listaDatas = new ArrayList();
 		
 		
-		//Adicionando dados na lista para fins de teste
-		//datas.listaDatas.add("Fri Jan 07 15:30:00 GMT 2024");
+		
 		
 
 	}
--- 08a12deJaneiro/Exercises/DadosEstatisticos/src/dadosestatisticos/DatasDeNascimento.java
+++ 08a12deJaneiro/Exercises/DadosEstatisticos/src/dadosestatisticos/DatasDeNascimento.java
@@ -5,47 +5,58 @@ import java.util.Collections;
 import java.util.Date;
 
 public class DatasDeNascimento implements DadosEstatisticos {
-	
+
 	private Date dataDeNascimento;
-	public ArrayList<Date> listaDatas;
-	
+	ArrayList<Date> listaDatas = new ArrayList<>();
+
 	public Date getDataDeNascimento() {
 		return dataDeNascimento;
 	}
 
-	public ArrayList<Date> getListaDatas() {
-		return listaDatas;
-	}
-
 	public Object max() {
-		
-		Date maior = listaDatas.get(0); 
-		
-		for (Date data : listaDatas){
+
+		Date maior = listaDatas.get(0);
+
+		for (Date data : listaDatas) {
 			if (data.after(maior)) {
 				maior = data;
 			}
 		}
 		return maior;
-		
+
 	}
-		
+
 	public Object min() {
-		
-		Date menor = listaDatas.get(0); 
-		
-		for (Date data : listaDatas){
+
+		Date menor = listaDatas.get(0);
+
+		for (Date data : listaDatas) {
 			if (data.before(menor)) {
 				menor = data;
 			}
 		}
 		return menor;
 	};
-	
+
 	public void ordernar() {
+
+		System.out.println("Datas antes da ordenação: ");
+		System.out.println("");
+
+		for (Date data : listaDatas) {
+			System.out.println(data);
+		}
 		Collections.sort(listaDatas);
+		System.out.println("");
+		System.out.println("Datas depois da ordenação: ");
+		System.out.println("");
+
+		for (Date data : listaDatas) {
+			System.out.println(data);
+		}
+		System.out.println("");
 	};
-	
+
 	public Object buscar(Object dataProcurada) {
 		for (Date data : listaDatas) {
 			if (data.equals(dataProcurada)) {
@@ -56,6 +67,5 @@ public class DatasDeNascimento implements DadosEstatisticos {
 		System.out.println("Data não encontrada.");
 		return null;
 	};
-
 
 }
