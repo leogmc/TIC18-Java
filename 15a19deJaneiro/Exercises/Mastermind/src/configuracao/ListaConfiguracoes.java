736526bcd428a17266efe8043019d1c9d0251929
leogmc <ifbaianoleo@gmail.com>
2024-01-25 13:22:40 -0300

    Subindo continuação dos testes

--- 15a19deJaneiro/Exercises/Mastermind/src/configuracao/ListaConfiguracoes.java
+++ 15a19deJaneiro/Exercises/Mastermind/src/configuracao/ListaConfiguracoes.java
@@ -1,5 +1,50 @@
 package configuracao;
 
+import java.io.File;
+import java.util.ArrayList;
+
 public class ListaConfiguracoes {
 
+	public static final String CAMINHO = "." + File.separator + "arquivos" + File.separator;
+
+	private String nomeArquivo;
+	private ArrayList<Configuracao> lista;
+
+	public String getNomeArquivo() {
+		return nomeArquivo;
+	}
+
+	public void setNomeArquivo(String nomeArquivo) {
+		this.nomeArquivo = nomeArquivo;
+	}
+
+	public ArrayList<Configuracao> getLista() {
+		return lista;
+	}
+
+	public void setLista(ArrayList<Configuracao> lista) {
+		this.lista = lista;
+	}
+
+	public ListaConfiguracoes() {
+		super();
+		nomeArquivo = CAMINHO + "configs.json";
+		lista = new ArrayList<Configuracao>();
+	}
+
+	public void novaConfiguracao(Configuracao configuracao) throws Exception {
+
+		for(Configuracao c : lista) {
+			if (c.getNome() == configuracao.getNome()) {
+				Exception e = new Exception("Já existe uma configuração chamada " + configuracao.getNome());
+				throw e;
+			}
+		}
+		
+		lista.add(configuracao);
+		
+	}
+
+	
+	
 }
--- 15a19deJaneiro/Exercises/Mastermind/src/configuracao/TestConfiguracao.java
+++ 15a19deJaneiro/Exercises/Mastermind/src/configuracao/TestConfiguracao.java
@@ -11,7 +11,7 @@ class TestConfiguracao {
 		Configuracao configuracao = new Configuracao();
 		
 		//Caso geral: salvar uma senha válida
-		String alfabeto = "ABCDEFGHIJ";
+		String alfabeto = "abcd";
 		
 		try {
 			configuracao.setAlfabeto(alfabeto);
@@ -47,12 +47,16 @@ class TestConfiguracao {
 			//não pode ter aceitado esse alfabeto:
 			assertNotEquals("a", configuracao.getAlfabeto());
 			
-			
-			//DEFININDO TAMANHO DA SENHA
-			
 		
+	}
+			
+	//DEFININDO TAMANHO DA SENHA	
+	@Test
+	void testSetTamanhoSenha() {
+		Configuracao configuracao = new Configuracao();
+	
 			//definindo um alfabeto (o teste do método que define o alfabeto já existe!)
-			alfabeto = "abcd";
+			String alfabeto = "abcd";
 			try {
 				configuracao.setAlfabeto(alfabeto);
 			} catch (Exception e) {
@@ -80,21 +84,80 @@ class TestConfiguracao {
 			}
 			//o tamanho da senha não pode ter sido aceito
 			assertNotEquals(tam, configuracao.getTamanhoSenha());
+			
+			
+			//Caso 1 versão 2: tentar inserir uma senha de tamanho 0
+			tam = 0;
+			try {
+				configuracao.setTamanhoSenha(tam);
+			} catch (Exception e) {
+				assertEquals("Senha deve ter ao menos 1 caracter", e.getMessage());
+			}
+			//o tamanho da senha não pode ter sido aceito
+			assertNotEquals(tam, configuracao.getTamanhoSenha());
+					
+			
+			//Caso 2 versão 2: inserir com sucesso senha de tamanho iguao ao alfabeto (que tem 4 caracteres)
+			tam = 4;
+			try {
+				configuracao.setTamanhoSenha(tam);
+			} catch (Exception e) {
+				fail("Não era para gerar exception aqui");
+			}
+			//o tamanho da senha precisa ter sido aceito
+			assertEquals(tam, configuracao.getTamanhoSenha());
+			
+			//Caso 2 versão 3: tentar inserir senha de tamanho maior que o alfabeto (que agora tem 10 caracteres)
+			try {
+				configuracao.setAlfabeto("1234567890");
+			} catch (Exception e1) {
+				e1.printStackTrace();
+			}
+			
+			tam = 11;
+			try {
+				configuracao.setTamanhoSenha(tam);
+			} catch (Exception e2) {
+				assertEquals("Senha não pode ser maior que o alfabeto (10 caracteres)", e2.getMessage());
+			}
+			//o tamanho da senha não pode ter sido aceito
+			assertNotEquals(tam, configuracao.getTamanhoSenha());
+			
+	}
+	
+	//DEFININDO TAMANHO MÁXIMO DE TENTATIVAS
+	@Test
+	void testSetMaxTentativas() {
 		
+		//CONFIGURAR LIMITE DE TENTATIVAS
 		
+		Configuracao configuracao = new Configuracao();
 		
+		//Caso geral: Inserindo um limite válido
+		int tentativas = 5;
 		
+		try {
+			configuracao.setMaxTentativas(tentativas);
+		} catch (Exception e1) {
+			fail("Gerou excecao indevida");
+			e1.printStackTrace();
+		}
 		
+		assertEquals(tentativas, configuracao.getMaxTentativas());
 		
+		//Caso 1: Inserindo um limite de tentativas menor ou igual a 1
 		
+		tentativas = 1;
 		
-		
-		
-		
-		
-		
-		
-		
+		try {
+			configuracao.setMaxTentativas(tentativas);
+		} catch (Exception e) {
+			assertEquals("O número de tentativas deve ser pelo menor maior que 1", e.getMessage() );
+		}									
 	}
 
 }
+		
+		
+
+
